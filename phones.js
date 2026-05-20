86cedc84d2c6c4485901c13cd8c8cafada9d8b3c
a13425063198-lang <a13425063198@gmail.com>
2026-05-21 06:27:11 +0800

    Add initial phone data to phones.js

---
+++ phones.js
@@ -0,0 +1,7 @@
+[
+{"brand":"小米","name":"小米13","price":1299,"spec":"骁龙8 Gen2 12+256GB 徕卡影像"},
+{"brand":"红米","name":"红米K70","price":899,"spec":"骁龙8 Gen2 12+256GB 5500mAh大电池"},
+{"brand":"苹果","name":"iPhone12","price":1199,"spec":"A14芯片 128GB 流畅耐用"},
+{"brand":"OPPO","name":"OPPO Find X7","price":2300,"spec":"天玑9300 哈苏全焦段影像"},
+{"brand":"vivo","name":"vivo X100","price":2100,"spec":"天玑9300 蔡司人像长焦"}
+]
